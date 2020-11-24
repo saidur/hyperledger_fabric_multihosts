cf06d2e12401c471dee940604aa446b6c882cab8
bijon <thinker.bijon@gmail.com>
2020-11-25 01:48:54 +0600

    multifabric

---
+++ artifacts/src/github.com/fabcar/javascript/index.js
@@ -0,0 +1,19 @@
+/*
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+// const FabCar = require('./lib/fabcar');
+//studentContract
+
+const Student = require('./lib/student');
+module.exports.Student = Student;
+module.exports.contracts = [ Student ];
+
+
+//const FabCar = require('./lib/fabcar');
+//module.exports.FabCar = FabCar;
+//module.exports.contracts = [ FabCar ];
